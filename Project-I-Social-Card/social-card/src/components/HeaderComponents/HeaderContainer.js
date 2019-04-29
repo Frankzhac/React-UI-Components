682a9352b7e615f13889f9301ac5dd3ca716b9aa
Frankzhac <Frank.hemle@gmail.com>
2019-04-29 18:00:23 -0400

    Header components done

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import './Header.css';
+// Imported HeaderTitle, ImageThumbnail, and HeaderContent
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = () => {
+    return (
+        <div className = 'header-container'>
+            <div>
+                <ImageThumbnail />
+            </div>
+            <div className = 'header-text'>
+                <HeaderTitle />
+                <HeaderContent />
+            </div>
+        </div>
+    );
+}
+
+export default HeaderContainer;
