ab7b1d8f233f05657cec5e02572b9d4009728a90
Frankzhac <Frank.hemle@gmail.com>
2019-04-30 18:18:04 -0400

    display components done and Button list created

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
+import {Fragment} from 'react'
 import './Display.css';
+
+const CalculatorDisplay = props => {
+    return (
+        <Fragment>
+            <div className = 'display'>0</div>
+        </Fragment>
+    );
+}
+
+export default CalculatorDisplay;
